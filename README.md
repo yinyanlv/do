1c502a071c532424d9477f258423c5fa0ee5fff5
yinyanlv <yanlv.yin@gmail.com>
2019-02-18 15:55:50 +0800

    add: README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# do-server
+
+write in rust.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate actix_web;
+
+use actix_web::{server, App, HttpRequest, Responder};
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(greet))
+            .resource("/{name}", |r| r.f(greet))
+    })
+        .bind("127.0.0.1:3200")
+        .expect("Can not bind to port 3200")
+        .run();
+}
+
+fn greet(req: &HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+
+    format!("Hello {}", name)
 }
